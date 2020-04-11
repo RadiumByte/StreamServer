5fdbd492e304b6bb037e1eee096591ffe015bf1b
RadiumByte <anton.fedyashov@gmail.com>
2020-04-11 17:23:48 +0300

    RTSP stream test

--- app/app.go
+++ app/app.go
@@ -1,14 +1,21 @@
 package app
 
+import (
+	"errors"
+	"sync"
+)
+
 // StreamManager is an interface for application's core
 type StreamManager interface {
 	AddCamera(camera CameraData)
 	SelectCamera(name string) error
 	GetCameras() []CameraData
+	GetActive() CameraData
 }
 
 // YoutubeAccessLayer is an interface for calling Youtube from Application
 type YoutubeAccessLayer interface {
+	RunRTMP(rtspInput string, rtmpOutput string)
 }
 
 // Application is responsible for all logics and communicates with other layers
@@ -17,23 +24,48 @@ type Application struct {
 	errc    chan<- error
 
 	// Storage for cameras
-	cameras []CameraData
+	cameras      []CameraData
+	activeCamera CameraData
+
+	camerasMutex sync.Mutex
 }
 
 // AddCamera creates new camera in list
 func (a *Application) AddCamera(camera CameraData) {
-	return
+	a.camerasMutex.Lock()
+	defer a.camerasMutex.Unlock()
+
+	a.cameras = append(a.cameras, camera)
 }
 
 // SelectCamera switches stream to specified camera
 // If specified camera is unknown - returns error
 func (a *Application) SelectCamera(name string) error {
-	return nil
+	a.camerasMutex.Lock()
+	defer a.camerasMutex.Unlock()
+
+	for i := range a.cameras {
+		if a.cameras[i].Name == name {
+			a.activeCamera.Name = a.cameras[i].Name
+			a.activeCamera.Type = a.cameras[i].Type
+			a.activeCamera.URL = a.cameras[i].URL
+
+			a.Youtube.RunRTMP(a.activeCamera.URL, "rtmp://a.rtmp.youtube.com/live2/uq80-f37c-z3c4-7rth")
+
+			return nil
+		}
+	}
+	return errors.New("Camera name is invalid")
 }
 
 // GetCameras returns list of added cameras
 func (a *Application) GetCameras() []CameraData {
-	return []CameraData{}
+	return a.cameras
+}
+
+// GetActive returns one active camera
+func (a *Application) GetActive() CameraData {
+	return a.activeCamera
 }
 
 // NewApplication constructs Application
@@ -43,6 +75,10 @@ func NewApplication(youtube YoutubeAccessLayer, errchannel chan<- error) *Applic
 	res.Youtube = youtube
 	res.errc = errchannel
 	res.cameras = []CameraData{}
+	res.activeCamera = CameraData{
+		Name: "",
+		URL:  "",
+		Type: 0}
 
 	return res
 }
--- yal/yal.go
+++ yal/yal.go
@@ -1,7 +1,19 @@
 package yal
 
+import (
+	"os/exec"
+)
+
 // YoutubeClient represents data for connection to Youtube
 type YoutubeClient struct {
+}
+
+// RunRTMP coverts input RTSP stream to RTMP stream using FFMPEG
+func (youtube *YoutubeClient) RunRTMP(rtspInput string, rtmpOutput string) {
+	var command string
+	command = "ffmpeg -f lavfi -i anullsrc -rtsp_transport tcp -i " + rtspInput + " -tune zerolatency -vcodec libx264 -t 12:00:00 -pix_fmt + -c:v copy -c:a aac -strict experimental -f flv " + rtmpOutput
+	cmd := exec.Command(command)
+	cmd.Run()
 }
 
 // NewYoutubeClient constructs object of YoutubeClient
@@ -11,3 +23,7 @@ func NewYoutubeClient() (*YoutubeClient, error) {
 
 	return res, nil
 }
+
+/*
+ffmpeg -f lavfi -i anullsrc -rtsp_transport tcp -i rtsp://81.23.197.208/user=admin_password=8555_channel=4_stream=0.sdp -tune zerolatency -vcodec libx264 -t 12:00:00 -pix_fmt + -c:v copy -c:a aac -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/uq80-f37c-z3c4-7rth
+*/
